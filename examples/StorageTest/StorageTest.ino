b285c7d3a2415bcce1693722866d40a96d1b396a
TANAKA Masayuki <tanakamasayuki@gmail.com>
2025-10-22 09:49:31 +0900

    Add initial implementation for StorageTest example with filesystem support

---
+++ examples/StorageTest/StorageTest.ino
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <SPIFFS.h>
+#include <FFat.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <ESP32SerialCtl.h>
+
+static esp32serialctl::ESP32SerialCtl<> esp32SerialCtl;
+
+static const int PIN_SD_CS = 4;
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin();
+    Wire1.begin();
+    delay(500);
+
+    // --- SPIFFS ---
+    bool ok_spiffs = SPIFFS.begin(/*formatOnFail=*/true, /*basePath=*/"/spiffs",
+                                  /*maxOpenFiles=*/10, /*partitionLabel=*/"spiffs");
+    Serial.printf("SPIFFS mount  : %s\n", ok_spiffs ? "OK" : "FAIL");
+
+    // --- LittleFS ---
+    bool ok_lfs = LittleFS.begin(/*formatOnFail=*/true, /*basePath=*/"/littlefs",
+                                 /*maxOpenFiles=*/10, /*partitionLabel=*/"littlefs");
+    Serial.printf("LittleFS mount: %s\n", ok_lfs ? "OK" : "FAIL");
+
+    // --- FFat (FATFS on flash) ---
+    bool ok_ffat = FFat.begin(/*formatOnFail=*/true, /*basePath=*/"/ffat",
+                              /*maxOpenFiles=*/10, /*partitionLabel=*/"ffat");
+    Serial.printf("FFat mount    : %s\n", ok_ffat ? "OK" : "FAIL");
+
+    // --- SD (SDSPI) ---
+    SPI.begin();
+    bool ok_sd = SD.begin(PIN_SD_CS, SPI, 40000000 /*max SPI clock*/);
+    Serial.printf("SD mount      : %s\n", ok_sd ? "OK" : "FAIL");
+}
+
+void loop()
+{
+    esp32SerialCtl.service();
+    delay(1);
+}
